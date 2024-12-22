38046fb4214fb43dca937732b02606049cb096d1
streaxxx69 <cerkas132@gmail.com>
2024-12-22 15:48:53 +0300

    add

--- 8_3/world.py
+++ 8_3/world.py
@@ -26,11 +26,32 @@ _map = []
 AIR = 'a'
 
 
+
+
+def load_map(file_name):
+    global _map
+    _map = []
+    with open (file_name) as f:
+
+        i= 0
+        for line in f:
+            blocks = line.strip()
+            row = []
+            for j in range (len(blocks)):
+                 cell = _Cell(_canvas, blocks[j], j * BLOCK_SIZE, i * BLOCK_SIZE)
+                 row.append(cell)
+            _map.append(row)
+            i+=1
+
+
 def get_block(row, col):
-    if row < 0 or col < 0 or row >= get_rows() or col >= get_cols():
-        return AIR
-    else:
+    # if row < 0 or col < 0 or row >= get_rows() or col >= get_cols():
+    #     return AIR
+    # else:
+    #     return _map[row][col].get_block()
+    if _inside_of_map(row,col):
         return _map[row][col].get_block()
+    return AIR
 
 
 def get_width():
@@ -71,6 +92,7 @@ def initialize(canv):
     global _canvas
     _canvas = canv
     create_map(25,25)
+    load_map(f'../map/2.tmap')
 
 def create_map(rows = 20, cols = 20):
     global _map
@@ -129,11 +151,17 @@ def get_cols():
 
 
 def update_cell(row, col):
-    if row < 0 or col < 0 or row >= get_rows() or col >= get_cols():
-        return
-    _map[row][col].update()
+    if _inside_of_map(row,col):
+        _map[row][col].update()
 
-
+def _inside_of_map(row,col):
+    if row < 1 or col < 1 or row >= get_rows()-1 or col >= get_cols()-1:
+        return False
+    return _map[row][col].destroy()
+def destroy(row, col):
+    if row < 1 or col < 1 or row >= get_rows()-1 or col >= get_cols()-1:
+        return False
+    return _map[row][col].destroy()
 class _Cell:
     def __init__(self, canvas, block,x,y):
         self.__canvas = canvas
@@ -145,6 +173,16 @@ class _Cell:
         self.__x = x
         self.__y = y
         self.__create_element(block)
+    def set_block(self, block):
+        if self.__block == block:
+            return
+        elif block == GROUND:
+            self.__delete_element()
+        elif self.__block == GROUND:
+            self.__create_element(block)
+        else:
+            self.itenconfig(self.__id, image=texture.get(block))
+        self.__block = block
 
     def __create_element(self, block):
         if block != GROUND:
@@ -152,11 +190,28 @@ class _Cell:
                                                    image=texture.get(block),
                                                    anchor=NW)
 
-    def __del__(self):
+    def __delete_element(self):
         try:
             self.__canvas.delete(self.__id)
         except:
             pass
+
+    def __del__(self):
+        # try:
+        #     self.__canvas.delete(self.__id)
+        # except:
+        #     pass
+         self.__delete_element()
+
+    def destroy(self):
+        if self.get_block() == BRICK:
+            self.set_block(GROUND)
+            return True
+        return False
+
+
+
+
 
     def get_block(self):
         return self.__block
