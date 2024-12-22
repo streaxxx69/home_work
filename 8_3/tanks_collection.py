b3fe0ed9cd303fa532a7643a62e204a2ef1f9fff
streaxxx69 <cerkas132@gmail.com>
2024-12-22 14:19:30 +0300

    rfrfd

---
+++ 8_3/tanks_collection.py
@@ -0,0 +1,66 @@
+
+from random import randint
+from tank import Tank
+import world
+
+_tanks = []
+_canvas = None
+
+
+def initialize(canv):
+    global _canvas
+    _canvas = canv
+
+    # player = Tank(canvas = canv, x = world.BLOCK_SIZE*2, y = world.BLOCK_SIZE*4, ammo = 100, speed=2, bot = False)
+    # enemy = Tank(canvas = canv, x = world.BLOCK_SIZE*4, y = world.BLOCK_SIZE*6, ammo = 100, speed=2, bot = True)
+    # enemy.set_target(player)
+    # _tanks.append(player)
+    # _tanks.append(enemy)
+#2
+    spawn(False)
+    for i in range(5):
+        spawn(True).set_target(get_player())
+
+def get_player():
+    return _tanks[0]
+
+def update():
+    for tank in _tanks:
+        tank.update()
+        check_collision(tank)
+
+def check_collision(tank):
+    for other_tank in _tanks:
+        if tank == other_tank:
+            continue
+        if tank.intersects(other_tank):
+            return True
+    return False
+
+
+# def spawn_enemy():
+#     while True:
+#         pos_x = randint(200, 800)
+#         pos_y = randint(200, 600)
+#         t = Tank(_canvas, x=pos_x, y=pos_y, speed=1)
+#         if not check_collision(t):
+#             t.set_target(get_player())
+#             _tanks.append(t)
+#             return True
+#1
+def spawn(is_bot=True):
+    cols = world.get_cols()
+    rows = world.get_rows()
+
+    while True:
+        col = randint(1, cols-1)
+        row = randint(1, rows-1)
+
+        if world.get_block(row, col) != world.GROUND:
+            continue
+
+        t = Tank(_canvas, x=col*world.BLOCK_SIZE,
+                 y=row * world.BLOCK_SIZE, speed=2, bot=is_bot)
+        if not check_collision(t):
+            _tanks.append(t)
+            return t
